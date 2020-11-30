bd4ebb4c1efce2d0dddbc322605275c97a8e6fa0
Jordan Halterman <jordan.halterman@gmail.com>
2020-11-30 13:53:50 -0800

    Clean up compile-protos script

--- build/bin/compile-protos.sh
+++ build/bin/compile-protos.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 proto_path="./proto:${GOPATH}/src/github.com/gogo/protobuf/protobuf:${GOPATH}/src/github.com/gogo/protobuf:${GOPATH}/src"
-proto_import_paths="Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types"
 
 ### Documentation generation
 protoc --proto_path=$proto_path \
@@ -30,32 +29,43 @@ protoc --proto_path=$proto_path \
     proto/onos/topo/topo.proto
 
 ### Go Protobuf code generation
+go_import_paths="Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types"
+
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/e2sub/endpoint,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/e2sub/endpoint,plugins=grpc:./go \
     proto/onos/e2sub/endpoint/*.proto
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/e2sub/subscription,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/e2sub/subscription,plugins=grpc:./go \
     proto/onos/e2sub/subscription/*.proto
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/e2sub/task,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/e2sub/task,plugins=grpc:./go \
     proto/onos/e2sub/task/*.proto
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/e2t/admin,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/e2t/admin,plugins=grpc:./go \
     proto/onos/e2t/admin/*.proto
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/e2t/e2,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/e2t/e2,plugins=grpc:./go \
     proto/onos/e2t/e2/*.proto
 protoc --proto_path=$proto_path \
-    --gogofaster_out=$proto_import_paths,import_path=onos/topo,plugins=grpc:./go \
+    --gogofaster_out=$go_import_paths,import_path=onos/topo,plugins=grpc:./go \
     proto/onos/topo/*.proto
 
 ### Python Protobuf code generation
 #protoc --proto_path=$proto_path \
-#    --python_betterproto_out=$proto_import_paths,import_path=onos/e2sub,plugins=grpc:./python \
-#    proto/onos/e2sub/*.proto
+#    --python_betterproto_out=./python/onos/e2sub \
+#    proto/onos/e2sub/endpoint/*.proto
 #protoc --proto_path=$proto_path \
-#    --python_betterproto_out=$proto_import_paths,import_path=onos/e2t,plugins=grpc:./python \
-#    proto/onos/e2t/*.proto
+#    --python_betterproto_out=./python/onos/e2sub \
+#    proto/onos/e2sub/subscription/*.proto
 #protoc --proto_path=$proto_path \
-#    --python_betterproto_out=$proto_import_paths,import_path=onos/topo,plugins=grpc:./python \
+#    --python_betterproto_out=./python/onos/e2sub \
+#    proto/onos/e2sub/task/*.proto
+#protoc --proto_path=$proto_path \
+#    --python_betterproto_out=./python/onos/e2t \
+#    proto/onos/e2t/admin/*.proto
+#protoc --proto_path=$proto_path \
+#    --python_betterproto_out=./python/onos/e2t \
+#    proto/onos/e2t/e2/*.proto
+#protoc --proto_path=$proto_path \
+#    --python_betterproto_out=./python/onos/topo \
 #    proto/onos/topo/*.proto
