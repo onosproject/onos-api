64132efa5395f571f4b1cc4f2a28a241461b5785
Jordan Halterman <jordan.halterman@gmail.com>
2020-12-14 12:25:47 -0800

    Generate Golang mocks in separate test package

--- build/bin/generate-mocks.sh
+++ build/bin/generate-mocks.sh
@@ -5,7 +5,10 @@ files=$(find go -name "*.pb.go")
 for f in $files; do
     dir=$(dirname ${f})
     pkg=$(basename ${dir})
-    path=$(echo $f | cut -d '/' -f3-)
-    echo "Generating go/test/${path}/${pkg}_mock.go"
-    mockgen -source ${f} -destination go/test/${path}/${pkg}_mock.go -package ${pkg}
+    root=$(echo $dir | cut -d '/' -f-2)
+    path=$(echo $dir | cut -d '/' -f3-)
+    if [[ $root != "go/test" ]]; then
+        echo "Generating go/test/${path}/${pkg}_mock.go"
+        mockgen -source ${f} -destination go/test/${path}/${pkg}_mock.go -package ${pkg}
+    fi
 done
