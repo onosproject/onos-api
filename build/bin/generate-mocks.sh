aa1dd98d6705b4d43f2727555e5168e45d950e95
Jordan Halterman <jordan.halterman@gmail.com>
2020-12-14 12:10:07 -0800

    Move mocks to go/test package

--- build/bin/generate-mocks.sh
+++ build/bin/generate-mocks.sh
@@ -3,7 +3,9 @@
 files=$(find go -name "*.pb.go")
 
 for f in $files; do
-    dir=$(dirname $f)
-    pkg=$(basename $dir)
-    mockgen -source $f -destination $dir/mock/topo_mock.go -package $pkg
+    dir=$(dirname ${f})
+    pkg=$(basename ${dir})
+    path=$(echo $f | cut -d '/' -f3-)
+    echo "Generating go/test/${path}/${pkg}_mock.go"
+    mockgen -source ${f} -destination go/test/${path}/${pkg}_mock.go -package ${pkg}
 done
