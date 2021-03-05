265311be2478a2a878823f4312f2b1c86f72a67f
Thomas Vachuska <tom@opennetworking.org>
2021-03-05 08:48:38 -0800

    Added some asserts, unskipped a test with a note

--- go/onos/ransim/types/types_test.go
+++ go/onos/ransim/types/types_test.go
@@ -49,7 +49,7 @@ func TestPartialShiftBasics(t *testing.T) {
 	assert.Equal(t, ECGI(0xBBBCCCFFFFFA), ToECGI(plmnID, ToECI(enbID, cellID)))
 }
 
-func TestANotherPartialShiftBasics(t *testing.T) {
+func TestAnotherPartialShiftBasics(t *testing.T) {
 	plmnID := PlmnID(0x310071)
 	cellID := CellID(0x1A)
 	enbID := EnbID(0x0FFFF)
@@ -69,15 +69,16 @@ func TestPlmnID(t *testing.T) {
 }
 
 func TestTypes(t *testing.T) {
-	t.Skip()
-	plmnID := PlmnID(221)
-	cellID := CellID(192)
+	plmnID := PlmnID(0xbbbccc)
+	cellID := CellID(0x12)
 	enbID := EnbID(0xf8f8f)
 
 	eci := ToECI(enbID, cellID)
 	ecgi := ToECGI(plmnID, eci)
 	genbID := ToGEnbID(plmnID, enbID)
 
+	// NOTE: These work for the given values of cellID and enbID, but may fail with "lesser" values that result
+	// in shifts less than 8 or 28 bits respectively.
 	assert.Equal(t, cellID, GetCellID(uint64(ecgi)), "incorrect CID")
 	assert.Equal(t, plmnID, GetPlmnID(uint64(ecgi)), "incorrect PLMNID")
 	assert.Equal(t, eci, GetECI(uint64(ecgi)), "incorrect ECI")
@@ -87,6 +88,8 @@ func TestTypes(t *testing.T) {
 
 func TestSimValues(t *testing.T) {
 	plmnID := ToPlmnID("310", "170")
+	assert.Equal(t, PlmnID(0x130071), plmnID)
+
 	enb1 := EnbID(144470)
 	enb2 := EnbID(144471)
 	ecgi11 := ToECGI(plmnID, ToECI(enb1, CellID(1)))
