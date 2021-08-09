0cd9f2466d34928a1eaeb593159e5ded31ee5493
Shad Ansari <shad69@gmail.com>
2021-08-08 21:30:08 -0700

    MHO get cmds for ues and cells

--- python/onos/mho/__init__.py
+++ python/onos/mho/__init__.py
@@ -2,140 +2,92 @@
 # sources: onos/mho/mho.proto
 # plugin: python-betterproto
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
 
 import betterproto
 from betterproto.grpc.grpclib_server import ServiceBase
 import grpclib
 
 
-class MhoParamType(betterproto.Enum):
-    """MhoParamType is enumeration type of MHO parameters"""
-
-    ALL = 0
-    A3OFFSET = 1
-    HYSTERESIS = 2
-    TIMETOTRIGGER = 3
+@dataclass(eq=False, repr=False)
+class GetRequest(betterproto.Message):
+    pass
 
 
 @dataclass(eq=False, repr=False)
-class GetMhoParamRequest(betterproto.Message):
-    # hoParamType is a type of handover parameter
-    ho_param_type: "MhoParamType" = betterproto.enum_field(1)
+class UeList(betterproto.Message):
+    ues: List["Ue"] = betterproto.message_field(1)
 
 
 @dataclass(eq=False, repr=False)
-class GetMhoParamResponse(betterproto.Message):
-    # hoParamType is a type of handover parameter
-    ho_param_type: "MhoParamType" = betterproto.enum_field(1)
-    # A3-Offset value
-    a3_offset: int = betterproto.int32_field(2)
-    # Hysteresis value
-    hysteresis: int = betterproto.int32_field(3)
-    # Time-to-Trigger value
-    time_to_trigger: int = betterproto.int32_field(4)
+class CellList(betterproto.Message):
+    cells: List["Cell"] = betterproto.message_field(1)
 
 
 @dataclass(eq=False, repr=False)
-class SetMhoParamRequest(betterproto.Message):
-    # hoParamType is a type of handover parameter
-    ho_param_type: "MhoParamType" = betterproto.enum_field(1)
-    # A3-Offset value
-    a3_offset: int = betterproto.int32_field(2)
-    # Hysteresis value
-    hysteresis: int = betterproto.int32_field(3)
-    # Time-to-Trigger value
-    time_to_trigger: int = betterproto.int32_field(4)
+class Ue(betterproto.Message):
+    ue_id: str = betterproto.string_field(1)
+    rrc_state: str = betterproto.string_field(2)
+    cgi: str = betterproto.string_field(3)
 
 
 @dataclass(eq=False, repr=False)
-class SetMhoParamResponse(betterproto.Message):
-    # success is a result whether MHO param is set successfully or not
-    success: bool = betterproto.bool_field(1)
+class Cell(betterproto.Message):
+    cgi: str = betterproto.string_field(1)
+    num_ues: int = betterproto.int64_field(2)
+    cumulative_handovers_in: int = betterproto.int64_field(4)
+    cumulative_handovers_out: int = betterproto.int64_field(5)
 
 
 class MhoStub(betterproto.ServiceStub):
-    async def get_mho_params(
-        self, *, ho_param_type: "MhoParamType" = None
-    ) -> "GetMhoParamResponse":
+    async def get_ues(self) -> "UeList":
 
-        request = GetMhoParamRequest()
-        request.ho_param_type = ho_param_type
+        request = GetRequest()
 
-        return await self._unary_unary(
-            "/onos.mho.Mho/GetMhoParams", request, GetMhoParamResponse
-        )
+        return await self._unary_unary("/onos.mho.mho/GetUes", request, UeList)
 
-    async def set_mho_params(
-        self,
-        *,
-        ho_param_type: "MhoParamType" = None,
-        a3_offset: int = 0,
-        hysteresis: int = 0,
-        time_to_trigger: int = 0,
-    ) -> "SetMhoParamResponse":
+    async def get_cells(self) -> "CellList":
 
-        request = SetMhoParamRequest()
-        request.ho_param_type = ho_param_type
-        request.a3_offset = a3_offset
-        request.hysteresis = hysteresis
-        request.time_to_trigger = time_to_trigger
+        request = GetRequest()
 
-        return await self._unary_unary(
-            "/onos.mho.Mho/SetMhoParams", request, SetMhoParamResponse
-        )
+        return await self._unary_unary("/onos.mho.mho/GetCells", request, CellList)
 
 
 class MhoBase(ServiceBase):
-    async def get_mho_params(
-        self, ho_param_type: "MhoParamType"
-    ) -> "GetMhoParamResponse":
+    async def get_ues(self) -> "UeList":
         raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
 
-    async def set_mho_params(
-        self,
-        ho_param_type: "MhoParamType",
-        a3_offset: int,
-        hysteresis: int,
-        time_to_trigger: int,
-    ) -> "SetMhoParamResponse":
+    async def get_cells(self) -> "CellList":
         raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
 
-    async def __rpc_get_mho_params(self, stream: grpclib.server.Stream) -> None:
+    async def __rpc_get_ues(self, stream: grpclib.server.Stream) -> None:
         request = await stream.recv_message()
 
-        request_kwargs = {
-            "ho_param_type": request.ho_param_type,
-        }
+        request_kwargs = {}
 
-        response = await self.get_mho_params(**request_kwargs)
+        response = await self.get_ues(**request_kwargs)
         await stream.send_message(response)
 
-    async def __rpc_set_mho_params(self, stream: grpclib.server.Stream) -> None:
+    async def __rpc_get_cells(self, stream: grpclib.server.Stream) -> None:
         request = await stream.recv_message()
 
-        request_kwargs = {
-            "ho_param_type": request.ho_param_type,
-            "a3_offset": request.a3_offset,
-            "hysteresis": request.hysteresis,
-            "time_to_trigger": request.time_to_trigger,
-        }
+        request_kwargs = {}
 
-        response = await self.set_mho_params(**request_kwargs)
+        response = await self.get_cells(**request_kwargs)
         await stream.send_message(response)
 
     def __mapping__(self) -> Dict[str, grpclib.const.Handler]:
         return {
-            "/onos.mho.Mho/GetMhoParams": grpclib.const.Handler(
-                self.__rpc_get_mho_params,
+            "/onos.mho.mho/GetUes": grpclib.const.Handler(
+                self.__rpc_get_ues,
                 grpclib.const.Cardinality.UNARY_UNARY,
-                GetMhoParamRequest,
-                GetMhoParamResponse,
+                GetRequest,
+                UeList,
             ),
-            "/onos.mho.Mho/SetMhoParams": grpclib.const.Handler(
-                self.__rpc_set_mho_params,
+            "/onos.mho.mho/GetCells": grpclib.const.Handler(
+                self.__rpc_get_cells,
                 grpclib.const.Cardinality.UNARY_UNARY,
-                SetMhoParamRequest,
-                SetMhoParamResponse,
+                GetRequest,
+                CellList,
             ),
         }
