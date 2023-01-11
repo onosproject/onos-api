07fa775ad1afe209da68f3d7026f9e2d1beb5c0d
Thomas Vachuska <tom.vachuska@intel.com>
2023-01-11 13:41:51 -0800

    Added PortKind

--- go/onos/topo/topo.go
+++ go/onos/topo/topo.go
@@ -63,6 +63,7 @@ const (
 	IPUKind          = "ipu"
 	HostKind         = "host"
 	RouterKind       = "router"
+	PortKind         = "port"
 	InterfaceKind    = "interface"
 	LinkKind         = "link"
 	ControllerKind   = "controller"
